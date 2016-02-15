4012ee66f3f550e18aa3f3bd1a6ea73996fb6687
scarfdestroyer <bradyrudesill@gmail.com>
2016-02-15 13:45:20 -0800

    More Hound Fixes

--- app/policies/organization_policy.rb
+++ app/policies/organization_policy.rb
@@ -23,11 +23,12 @@ class OrganizationPolicy
   private
 
     def organization_member_for_user
-      @organization_member_for_user ||= OrganizationMembership.find_by(member: user, organization: organization)
+      @organization_member_for_user ||= 
+      OrganizationMembership.find_by(member: user, organization: organization)
     end
 
     def current_user_is_at_least_admin_in_organization?
       return false unless organization_member_for_user
-      return true if organization_member_for_user.admin? or organization_member_for_user.owner?
+      return true if organization_member_for_user.admin? || organization_member_for_user.owner?
     end
 end
